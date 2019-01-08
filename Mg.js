31242dade273e9a67e8241ee33f579d930ed2ef1
Saheed Oladosu <oladosu777@gmail.com>
2019-01-08 22:49:48 +0100

    more more chan

--- Mg.js
+++ Mg.js
@@ -2,6 +2,38 @@
 function username() {
 	let wname = document.getElementById('usename').value;
 	document.getElementById('mi').innerHTML =`Hey ${wname}! Please select your Magic Number`
+}
+
+
+const compare2 = () =>{
+	let randomNumber = Math.floor(Math.random()*8);
+	let eightBall = '';
+	let myNum = document.getElementById('numValue').value;
+	if(randomNumber == myNum){
+		switch (randomNumber) {
+  case 0:
+  	eightBall = 'It is certain';
+  	break;
+  case 1:
+  	eightBall = 'It is decidedly so';
+  	break;
+  case 3:
+  	eightBall = 'Cannot predict now';
+  	break;
+  case 4:
+  	eightBall = 'Do not count on it';
+  	break;
+  case 5:
+  	eightBall = 'My sources say no';
+  	break;
+  case 6:
+  	eightBall = 'Outlook not so good';
+  	break;
+  case 7:
+  	eightBall = 'Signs point to yes';
+  	break;
 	}
 
-
+	}
+	document.getElementById('root').innerHTML = eightBall;
+}
