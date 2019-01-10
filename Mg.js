1be4a3cf0001f68ca8140aba75f0d1c2a8537cf7
Saheed Oladosu <oladosu777@gmail.com>
2019-01-10 11:57:04 +0100

    Commit for today

--- Mg.js
+++ Mg.js
@@ -10,32 +10,10 @@ function compare2(){
 	let eightBall = '';
 	let myNum = document.getElementById('numValue').value;
 	if(randomNumber === myNum){
-		switch (randomNumber) {
-  case 0:
-  	eightBall = 'It is certain';
-  	break;
-  case 1:
-  	eightBall = 'It is decidedly so';
-  	break;
-  case 3:
-  	eightBall = 'Cannot predict now';
-  	break;
-  case 4:
-  	eightBall = 'Do not count on it';
-  	break;
-  case 5:
-  	eightBall = 'My sources say no';
-  	break;
-  case 6:
-  	eightBall = 'Outlook not so good';
-  	break;
-  case 7:
-  	eightBall = 'Signs point to yes';
-  	break;
-	}
+		console.log("correct number")
 	
 	}else{
-		
+		console.log("Wrong number")
 	}
 }
 console.log(document.getElementById('root').innerHTML = eightBall);	
