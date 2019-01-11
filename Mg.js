e982158176f447ea58ee0addfda053683808a4cc
Saheed Oladosu <oladosu777@gmail.com>
2019-01-11 18:17:07 +0100

    changed switch statement to if/else

--- Mg.js
+++ Mg.js
@@ -5,45 +5,10 @@ function username() {
 }
 
 function compare2(){
-	//let randomNumber = Math.floor(Math.random()*8);
+	let randomNumber = Math.floor(Math.random()*8);
 	let eightBall = ' ';
 	let myNum = document.getElementById('numValue').value;
-	if(myNum === 0){
-		alert('0')
-	}else if(myNum===1){
-		alert('1')
-	}else if(myNum===2){
-		alert('2')
-	}else if(myNum===3){
-		alert('3')
-	}
-	/*switch (myNum) {
-  case 0:
-  	eightBall = 'It is certain';
-  	break;
-  case 1:
-  	eightBall = 'It is decidedly so';
-  	break;
-  case 3:
-  	alert( 'Cannot predict now' );
-  	break;
-  case 4:
-  	eightBall = 'Do not count on it';
-  	break;
-  case 5:
-  	eightBall = 'My sources say no';
-  	break;
-  case 6:
-  	eightBall = 'Outlook not so good';
-  	break;
-  case 7:
-  	eightBall = 'Signs point to yes';
-  	break;
-	}
-
-	console.log(eightBall);
-
-	/*console.log(randomNumber);
+	
 	if(randomNumber == myNum){
 		console.log("correct number")
 	
@@ -51,6 +16,5 @@ function compare2(){
 		console.log("Wrong number")
 	}
 	console.log(myNum);
-	console.log(randomNumber);*/
-}
+	console.log(randomNumber);
 
