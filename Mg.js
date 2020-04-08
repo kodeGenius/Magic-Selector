51268565e1d319981f77b6ff47d0443725b286f2
Saheed Oladosu <38882296+kodeGenius@users.noreply.github.com>
2020-04-08 23:32:58 +0100

    fht edit

--- Mg.js
+++ Mg.js
@@ -1,4 +1,4 @@
-// magic
+// magi
 function username() {
 	let wname = document.getElementById('usename').value;
 	document.getElementById('mi').innerHTML =`Hey ${wname}! Please select your Magic Number`
